3be0a34aa1d9243388ec2d01908aca3aa31f7ba4
dantunez49 <31718436+dantunez49@users.noreply.github.com>
2017-09-07 01:54:28 -0700

    Add files via upload

---
+++ HelloSAC.java
@@ -0,0 +1,8 @@
+public class HelloSAC {
+
+    public static void main(String[] args) {
+        // Prints "Hello, Santa Ana College!" to the terminal window.
+        System.out.println("Hello, Santa Ana College!");
+    }
+
+}
